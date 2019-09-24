10e295a8d4a8b8c27adb89f94ea5804b22cd3050
Marco Romanelli <marco@MBP-di-Marco.homenet.telecomitalia.it>
2019-09-24 16:12:10 +0200

    improve docstrings and comments; change admin descriptors for Answer and Reservation

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
@@ -7,6 +9,7 @@ from .forms import PlayerCreationForm, PlayerChangeForm
 
 
 class PlayerAdmin(BaseUserAdmin):
+    """Define a new (Player) Admin."""
     # The forms to add and change user instances
     form = PlayerChangeForm
     add_form = PlayerCreationForm
@@ -39,47 +42,61 @@ class PlayerAdmin(BaseUserAdmin):
 # Now register the new UserAdmin...
 admin.site.register(Player, PlayerAdmin)
 
-# ... and, since we're not using Django's built-in permissions,
+# and, since we're not using Django's built-in permissions,
 # unregister the Group model from admin.
 admin.site.unregister(Group)
 
 
 class ReservationInline(admin.TabularInline):
+    """
+    Define an inline admin descriptor for :model:`quiz.Reservation` model.
+    This will be used to show all the question workflow (list reservation,
+    approve, list answer, approve) on the question admin page.
+    """
     model = Reservation
     readonly_fields = ('question', 'player', 'reservation_date', )
 
     # Order by reservation date
     ordering = ('reservation_date',)
 
-    def has_add_permission(self, request):
-        """Admin cannot add reservations. """
+    def has_add_permission(self, request, obj=None):
+        """Admin should not add reservations."""
         return False
 
     def has_delete_permission(self, request, obj=None):
-        """Admin cannot delete reservations. """
+        """Admin should not delete reservations. """
         return False
 
 
 class AnswerInline(admin.StackedInline):
+    """
+    Define an inline admin descriptor for :model:`quiz.Answer` model.
+    This will be used to show all the question workflow (list reservation,
+    approve, list answer, approve) on the question admin page.
+    """
     model = Answer
     readonly_fields = ('question', 'player', 'creation_date', )
 
     # Order by reservation date
     ordering = ('creation_date',)
 
-    def has_add_permission(self, request):
-        """Admin cannot add answers. """
+    def has_add_permission(self,  request, obj=None):
+        """Admin should not add answers."""
         return False
 
     def has_delete_permission(self, request, obj=None):
-        """Admin cannot delete reservations. """
+        """Admin should not add answers. """
         return False
 
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    """Tells Django the options we want when you register the object. """
-    #fields = ('question_text', 'creation_date', 'status',)
+    """
+    Define an admin descriptor for :model:`quiz.Question` model.
+    Almost the entire question workflow works around this descriptor.
+    Withing this, we list also inline descriptors for
+    :model:`quiz.Reservation` and :model:`quiz.Answer`.
+    """
     fieldsets = (
         (None, {
             'fields': ('question_text', 'status'),
@@ -92,13 +109,13 @@ class QuestionAdmin(admin.ModelAdmin):
 
     # Control which fields are displayed on the change list page
     # note: By default, Django displays the str() of each object
-    list_display = ('question_text', 'creation_date', 'status',)
+    list_display = ('id', 'cropped_question_text', 'creation_date', 'status',)
 
     # Activate filters in the right sidebar of the change list page
     list_filter = ['creation_date', 'status']
 
     # Enable a search box
-    #search_fields = ['question_text']
+    # search_fields = ['question_text']
 
     # Display as non-editable
     readonly_fields = ('creation_date',)
@@ -109,62 +126,82 @@ class QuestionAdmin(admin.ModelAdmin):
     # Make other models editable on the same page as a parent model
     inlines = [ReservationInline, AnswerInline]
 
+    def cropped_question_text(self, x):
+        """Returns a cropped question text at 30 chars."""
+        return x.question_text[:30]
+    cropped_question_text.short_description = 'question text'
+
 
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
-    """Tells Django the options we want when you register the object. """
-
-    readonly_fields = ('question', 'player', 'reservation_date',)
-
+    """
+    Define the (full) admin descriptor for :model:`quiz.Reservation` model.
+    """
     fieldsets = (
+        (None, {
+            'fields': ('approved',)
+        }),
         ('Linked to', {
             'classes': ('wide',),
             'fields': ('question', 'player'),
         }),
         ('Addition information', {
             'classes': ('wide',),
-            'fields': ('reservation_date', 'approved'),
+            'fields': ('reservation_date',),
         }),
     )
 
-    list_display = ('id', 'question_text', 'player', 'approved',)
+    list_display = ('id', 'cropped_question_text', 'player', 'approved',)
+
+    readonly_fields = ('question', 'player', 'reservation_date',)
 
     # Order by creation_date
     ordering = ('reservation_date',)
 
-    def question_text(self, x):
-        return x.question.question_text
-    question_text.short_description = 'question text'
+    def cropped_question_text(self, x):
+        """Returns a cropped question text at 30 chars."""
+        return x.question.question_text[:30]
+    cropped_question_text.short_description = 'question text'
 
 
 @admin.register(Answer)
 class AnswerAdmin(admin.ModelAdmin):
-    """Tells Django the options we want when you register the object. """
-
-    readonly_fields = ('question', 'player', 'creation_date',)
-
+    """
+    Define the (full) admin descriptor for :model:`quiz.Answer` model.
+    """
     fieldsets = (
         (None, {
-            'fields': ('answer_text',)
+            'fields': ('answer_text', 'status')
         }),
-        ('Addition information', {
-            'classes': ('wide',),
-            'fields': ('creation_date', 'status'),
-        }),
-        ('Linked to', {
+        ('Referred to', {
             'classes': ('wide',),
             'fields': ('question', 'player'),
         }),
+        ('Addition information', {
+            'classes': ('wide',),
+            'fields': ('creation_date',),
+        }),
     )
 
-    list_display = ('id', 'question_text', 'player', 'status',)
+    readonly_fields = ('question', 'player', 'creation_date',)
+
+    list_display = ('id', 'cropped_answer_text', 'question_id',
+                    'player', 'status',)
 
     # Order by creation_date
     ordering = ('creation_date',)
 
-    def question_text(self, x):
-        return x.question.question_text
-    question_text.short_description = 'question text'
+    def cropped_answer_text(self, x):
+        """Returns a cropped answer text at 30 chars."""
+        return x.answer_text[:30]
+    cropped_answer_text.short_description = 'answer text'
+
+    def question_id(self, x):
+        """Returns a the :model:`quiz.Question` id."""
+        return x.question.id
+    question_id.short_description = 'question id'
+
+
 
 
 
--- quiz/forms.py
+++ quiz/forms.py
@@ -2,25 +2,33 @@
 
 from django import forms
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
-from django.utils.translation import gettext as _
 
 from .models import Player, Answer
 
 
 class PlayerCreationForm(forms.ModelForm):
     """
-    A form for creating new users. Includes all the required
-    fields, plus a repeated password.
+    A custom form for creating new :model:`quiz.Player` instance.
+    Includes all the required fields, plus a repeated password.
     """
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label='Password confirmation',
+        widget=forms.PasswordInput
+    )
 
     class Meta:
         model = Player
         fields = ('nickname',)
 
     def clean_password2(self):
-        # Check that the two password entries match
+        """
+        Check that the two password entries match and return
+        the verified password.
+        """
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
@@ -28,7 +36,10 @@ class PlayerCreationForm(forms.ModelForm):
         return password2
 
     def save(self, commit=True):
-        # Save the provided password in hashed format
+        """
+        Save the provided password in hashed format.
+        Return an instance of :model:`quiz.Player`.
+        """
         player = super().save(commit=False)
         player.set_password(self.cleaned_data["password1"])
 
@@ -37,11 +48,12 @@ class PlayerCreationForm(forms.ModelForm):
 
         return player
 
+
 class PlayerChangeForm(forms.ModelForm):
     """
-    A form for updating users. Includes all the fields on
-    the user, but replaces the password field with admin's
-    password hash display field.
+    A form for update a :model:`quiz.Player` instance.
+    Includes all the fields on the user, but replaces the password field
+    with admin's password hash display field.
     """
     password = ReadOnlyPasswordHashField()
 
@@ -50,19 +62,18 @@ class PlayerChangeForm(forms.ModelForm):
         fields = ('nickname', 'password', 'is_active', 'is_admin')
 
     def clean_password(self):
+        """Return the initial password."""
         # Regardless of what the user provides, return the initial value.
         # This is done here, rather than on the field, because the
         # field does not have access to the initial value
         return self.initial["password"]
 
-from crispy_forms.helper import FormHelper
 
 class AnswerCreationForm(forms.ModelForm):
     """
-    A form for creating new answers.
+    A custom form for creating new :model:`quiz.Answer` instances.
     Includes all the required fields.
     """
-
     answer_text = forms.CharField(
         label='Answer',
         widget=forms.Textarea(attrs={'rows':4}),
@@ -73,6 +84,10 @@ class AnswerCreationForm(forms.ModelForm):
         fields = ('answer_text',)
 
     def save(self, commit=True):
+        """
+        Override default Django save method.
+        Return a instance of :model:`quiz.Answer`.
+        """
         answer = super().save(commit=False)
 
         if commit:
